911288baee7c26ed748b265c46cf4fca2f8505a5
Alex Felipe <alexfelipevieira@gmail.com>
2018-06-14 10:42:34 -0300

    vídeo 3.1

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class LeilaoTest {
 
     @Test
-    public void getDescricao() {
+    public void getDescricaoQuandoRecebeDescricaoDevolveDescricao() {
         // criar cenário de teste
         Leilao console = new Leilao("Console");
 
@@ -18,14 +18,38 @@ public class LeilaoTest {
         assertEquals("Console", descricaoDevolvida);
     }
 
+    //[nome do método][Estado de teste][resultado esperado]
+
     @Test
-    public void getMaiorLance(){
+    public void getMaiorLanceQuandoRecebeApenasUmLanceDevolveMaiorLance(){
         Leilao console = new Leilao("Console");
         console.propoe(new Lance(new Usuario("Alex"), 200.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvidoDoConsole = console.getMaiorLance();
 
-        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+        assertEquals(200.0, maiorLanceDevolvidoDoConsole, 0.0001);
+    }
+
+    @Test
+    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemCrescenteDevolveMaiorLance(){
+        Leilao computador = new Leilao("Computador");
+        computador.propoe(new Lance(new Usuario("Alex"), 100.0));
+        computador.propoe(new Lance(new Usuario("Fran"), 200.0));
+
+        double maiorLanceDevolvidoDoComputador = computador.getMaiorLance();
+
+        assertEquals(200.0, maiorLanceDevolvidoDoComputador, 0.0001);
+    }
+
+    @Test
+    public void getMaiorLanceQuandoRecebeMaisDeUmLanceEmOrdemDecrescenteDevolveMaiorLance(){
+        Leilao carro = new Leilao("Carro");
+        carro.propoe(new Lance(new Usuario("Alex"), 10000.0));
+        carro.propoe(new Lance(new Usuario("Fran"), 9000.0));
+
+        double maiorLanceDevolvidoDoCarro = carro.getMaiorLance();
+
+        assertEquals(10000.0, maiorLanceDevolvidoDoCarro, 0.0001);
     }
 
 }
